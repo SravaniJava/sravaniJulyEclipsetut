1dc361fe4f634b8fe2d7be74e7f6a77117c1542c
SravaniJava <sravanigoudjava@gmail.com>
2024-07-08 12:05:41 +0530

    "2nd time testing"

--- eclipsegitEg/src/main/java/nit/eclipseGitEg/App.java
+++ eclipsegitEg/src/main/java/nit/eclipseGitEg/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("working with eclipse git");
+        System.out.println("testing not yet done properly");
         }
 }
